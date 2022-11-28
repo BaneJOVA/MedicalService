f130f3dedf1ed4e134761b94ce38495c13b52a60
QA36 <QA36@DESKTOP-AESHFBQ>
2022-11-28 20:57:27 +0100

    Init

---
+++ MedicalService/MedicalService/MedicalTest.cs
@@ -0,0 +1,41 @@
+using MedicalService.Driver;
+using MedicalService.Page;
+
+namespace MedicalService.Page
+{
+    public class MedicalPage
+    {
+        LoginPage loginPage;
+        MedicalPages medicalPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            WebDrivers.Initialize();
+            loginPage = new LoginPage();
+            medicalPage = new MedicalPages();
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            WebDrivers.CleanUp();
+
+        }
+        [Test]
+        public void TC01_MakeAppointment_ShouldAppointmentBeCompleted()
+        {
+            loginPage.AppMedic.Click();
+            loginPage.Login("John Doe", "ThisIsNotAPassword");
+            medicalPage.SelectOption("Hongkong CURA Healthcare Center");
+            medicalPage.CheckBox.Click();
+            medicalPage.MedicId.Click();
+            medicalPage.Data.SendKeys("25/12/2022");
+            medicalPage.Comment.SendKeys("Zakazano");
+            medicalPage.Buton.Submit();
+
+            Assert.That("Appointment Confirmation",IsEqualTo(medicalPage.Confirm.Text));
+        }
+        
+    }
+}
---
+++ MedicalService/MedicalService/Page/MedicalPage.cs
@@ -0,0 +1,29 @@
+﻿using MedicalService.Driver;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace MedicalService.Page
+{
+    public class MedicalPages
+    {
+        private IWebDriver driver = WebDrivers.Instance;
+        private object text;
+        private object element;
+
+        public void SelectOption(string text)
+        {
+            SelectElement element = new SelectElement(driver.FindElement(By.Id("combo_facility")));
+            element.SelectByText(text);
+        }
+
+        public IWebElement CheckBox => driver.FindElement((By.Id("chk_hospotal_readmission")));
+        public IWebElement MedicId => driver.FindElement((By.Id("radio_program_medicaid")));
+        public IWebElement Data => driver.FindElement((By.Id("txt_visit_date")));
+        public IWebElement Comment => driver.FindElement((By.Id("txt_comment")));
+        public IWebElement Buton => driver.FindElement((By.Id("btn-book-appointment")));
+
+        public IWebElement Confirm => driver.FindElement((By.CssSelector("text-selectorh2")));
+
+    }
+
+}
